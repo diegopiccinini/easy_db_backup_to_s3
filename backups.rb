d0d00efc7a3639b122ec49dda4a65b586cb3572b
Diego Lagos <diego@myorb.com>
2020-03-11 15:13:03 +0000

    updated with the encryption and md5 backups

--- backups.rb
+++ backups.rb
@@ -22,10 +22,26 @@ def backups(data)
     conn_data = conns[conn]
     s = conn_data.each_pair.map { |k, v| "-#{k}#{v}" }
     file = "#{BASEDIR}/#{conn}/#{hour}/#{db}.sql"
-    s = "mysqldump #{s.join(' ')} #{db} > #{file}"
-    system s
-    system "gzip #{file}"
+    s = "mysqldump #{s.join(' ')} --skip-dump-date --skip-comments #{db} > #{file}"
+    final_tasks(s, file)
   end
+end
+
+def md5(file)
+  system "md5sum #{file} > #{file}.md5sum"
+end
+
+def gpg(file)
+  system "gpg --encrypt --sign --armor -r #{GPG_EMAIL} #{file}"
+end
+
+def final_tasks(s, file)
+  system s
+  md5(file)
+  gpg(file)
+  md5("#{file}.asc")
+  system "gzip #{file}.asc"
+  system "rm #{file}"
 end
 
 def upload_and_remove
@@ -40,13 +56,13 @@ def pg_backups(data)
     conn_data = conns[conn]
     file = "#{BASEDIR}/#{conn}/#{hour}/#{db}.sql"
     s = "PGPASSWORD=#{conn_data['password']} pg_dump -U#{conn_data['U']} -h#{conn_data['h']} -d#{db} > #{file}"
-    system s
-    system "gzip #{file}"
+    final_tasks(s, file)
   end
 end
 
 def main
   data = YAML.load_file "#{PROJECT_DIR}/config/databases.yml"
+  GPG_EMAIL = data['gpg_email']
   backups(data['mysql'])
   pg_backups(data['postgreSQL'])
   upload_and_remove
